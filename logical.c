629819eb9b4a7671a1cf2b507cf8d058c9fe5bdb
Constantin Radulescu <seaman105@yahoo.com>
2017-12-02 21:43:06 +0000

    Initial Commit

---
+++ logical.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+    {
+        char c = get_char();
+        if (c == 'y' || c == 'Y')
+        {
+            printf("yes\n");
+        }
+        else if (c == 'n' || c == 'N')
+        {
+        printf("no\n");
+        }
+        else
+        {
+            printf("error\n");
+        }
+    }
---
+++ overflowfinit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+    {
+        int n = o
+        for (int i = 0; i < 64; i++)
+        {
+            printf("n is %i\n", n)
+            n = n * 2;
+        }
+    }
